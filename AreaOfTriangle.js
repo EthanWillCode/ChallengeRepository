e17d2b8c3c368d6f219c922bbd3ca6228cd3417f
EthanWillCode <Aethil@yahoo.com>
2023-07-01 15:34:49 -0500

    function correctly returns the area of a triangle

---
+++ AreaOfTriangle.js
@@ -0,0 +1,21 @@
+function triArea(base, height) {
+	return (base*height)/2;
+}
+
+//should return 3
+let test1 = triArea(2,3);
+
+//should return 17.5
+let test2 = triArea(5,7);
+
+//should return 8
+let test3 = triArea(4,4);
+
+//should return 48
+let test4 = triArea(8,12);
+
+
+console.log(test1)
+console.log(test2)
+console.log(test3)
+console.log(test4)
